b1d620acf8f6852a872be9a3ec660c542f5376b6
Olga <olgastarynenko@gmail.com>
2019-08-13 15:14:57 +0300

    Update utils.py

--- meter/utils.py
+++ meter/utils.py
@@ -4,16 +4,21 @@ def consumption(records):
     records_ =records[:]
     value_list = [] # list with value
     date_list = [] # list with date
-
+    
     for x in records:
-        tmp = x.value
-        value_list.append(tmp)
+        date_list.append(x.date.isoformat())
+        value_list.append(x.value)
         records = x
+        
+#     for x in records:
+#         tmp = x.value
+#         value_list.append(tmp)
+#         records = x
 
-    for x in records_:
-        tmp = x.date.isoformat()
-        date_list.append(tmp)
-        records_ = x
+#     for x in records_:
+#         tmp = x.date.isoformat()
+#         date_list.append(tmp)
+#         records_ = x
 
     diff = [value_list[n - 1] - value_list[n] for n in range(1, len(value_list))] #Performing of calculation consumptions
     result_lst = list(zip(date_list, date_list[1:], diff))  # create list of tuples with consumption and date
